15ba3deded7eaafd88e6875ef2e540aa1b4571ea
Asim-A <asimabazi@hotmail.no>
2019-09-26 01:30:16 +0200

    nye modeller (?)

---
+++ Models/Billetter/VoksenBillett.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEBAPP.Models.Billetter
+{
+    public class VoksenBillett : Billett
+    {
+    }
+}
