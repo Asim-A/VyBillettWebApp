ff453d705ee4d959c78d0e487bbc160747720ffc
Asim-A <asimabazi@hotmail.no>
2019-09-26 01:37:26 +0200

    updated models

--- Models/Billetter/Billett.cs
+++ Models/Billetter/Billett.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace WEBAPP.Models
 {
-    public class Billett
+    public abstract class Billett
     {
+        [Key]
+        public int BilettId { get; set; }
+        public string strekning { get; set; }
+        public string dato { get; set; }
     }
 }
