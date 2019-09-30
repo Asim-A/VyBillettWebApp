c4228b1036745fdc0e6b62a94936667216c65633
Hamza Amir <Hamza_Aftab97@hotmail.com>
2019-09-30 15:55:33 +0200

    er mappen der

---
+++ Models/Databasemodeller/Strekning.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VyBillettWebApp.Models
+{ 
+
+    public class Strekning
+    {
+        string fra { get; set; }
+        string til { get; set; }
+    }
+}
