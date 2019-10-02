0e1e5ea29f02bef8161994ee90ee77a027562d35
Asim-A <asimabazi@hotmail.no>
2019-10-02 22:46:25 +0200

    oppdaterte modeller

--- Models/Logiskemodeller/Billett.cs
+++ Models/Logiskemodeller/Billett.cs
@@ -4,6 +4,10 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
+/// <summary>
+/// Klasser under "Logiskemodeller" mappen er "smarte" modeller som vi har tenkt til å bruker
+/// til senere oppgaver, akkurat nå har de ingen funksjon og er der bare for videreutvikling og struktur
+/// </summary>
 namespace VyBillettWebApp.Models
 {
     public abstract class Billett
