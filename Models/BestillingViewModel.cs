06a8506e8ae1ea77acaf4ae6e91fd872fb971a11
Hamza Amir <Hamza_Aftab97@hotmail.com>
2019-10-03 15:50:26 +0200

    endret på view og satt inn kalender¯\_(ツ)_/¯

--- Models/BestillingViewModel.cs
+++ Models/BestillingViewModel.cs
@@ -10,20 +10,25 @@ namespace VyBillettWebApp.Models
     {
         [Display(Name = "Fra")]
         [Required(ErrorMessage = "Du må velge hvor du skal reise fra")]
+        [RegularExpression("[a-zA-ZøæåØÆÅ]{2,16}", ErrorMessage = "æla psyko skriv riktig a")]
         public string fra { get; set; }
 
         [Display(Name = "Til")]
         [Required(ErrorMessage = "Du må velge hvor du skal reise til")]
+        [RegularExpression("[a-zA-ZøæåØÆÅ]{2,16}", ErrorMessage = "æla psyko skriv riktig a")]
         public string til { get; set; }
 
         [Display(Name = "Antall Barn")]
+        [Range(0, 10, ErrorMessage ="velg antall barn, maksgrense er 10 per bestilling")]
         public int antall_barn { get; set; }
 
         [Display(Name = "Antall Studenter")]
+        [Range(0, 10, ErrorMessage = "velg antall studenter, maksgrense er 10 per bestilling")]
         public int antall_studenter { get; set; }
 
-        [Display(Name = "Antall Vokse")]
-        public int antall_vokse { get; set; }
+        [Display(Name = "Antall Voksne")]
+        [Range(0, 10, ErrorMessage = "velg antall voksne, maksgrense er 10 per bestilling")]
+        public int antall_voksne { get; set; }
 
         [Display(Name = "Reise dato")]
         public DateTime reise_dato_tid { get; set; }        //TODO 
