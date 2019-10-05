aebbff996a783347d94a456f9ccbfd27ffd0bee5
Asim-A <asimabazi@hotmail.no>
2019-10-05 15:18:32 +0200

    cleaner js

---
+++ Scripts/homepage.js
@@ -0,0 +1,45 @@
+﻿var picker = new Pikaday(
+    {
+        field: document.getElementById('datepicker'),
+        minDate: new Date(),
+        onSelect: function () {
+            console.log(this);
+        }
+    });
+
+$(function () {
+    $.ajax({
+        url: '/Home/ajaxtest',
+        type: 'GET',
+        dataType: 'json',
+        success: function (n) {
+            alert(n);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+})
+
+$("#submit_buy_btn").click(function () {
+    var Inn = {
+        reise_dato_tid: $("#datepicker").val()
+    }
+
+    $.ajax({
+        url: '/Home/ajaxtestinn',
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(Inn),
+        contentType: "application/json;charset=utf-8",
+        success: function (i) {
+            alert(i);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+
+})
+
+
