26f159f7e8c4e44e0da2d5e1a20219e8ac22e113
Asim-A <asimabazi@hotmail.no>
2019-10-05 17:50:35 +0200

    dropdown med søk funker

--- Scripts/homepage.js
+++ Scripts/homepage.js
@@ -43,3 +43,104 @@ $("#submit_buy_btn").click(function () {
 })
 
 
+let fra_input = document.getElementById("fra");
+let til_input = document.getElementById("til");
+
+function setup_dropdown() {
+    setup_individual_dropdown("myDropdownFra");
+    setup_individual_dropdown("myDropdownTil");
+}
+
+function setup_individual_dropdown(id) {
+    let dropdown = document.getElementById(id);
+    let input;
+
+    if (id === "myDropdownFra") {
+        input = fra_input;
+    }
+
+    else {
+        input = til_input;
+    }
+
+    for (let i = 0; i < liste_stasjoner.length; i++) {
+        
+        let node = create_menu_item(liste_stasjoner[i], input, dropdown);
+        dropdown.appendChild(node);
+    }
+}
+
+function create_menu_item(value, input, dropdown) {
+    let node = document.createElement("a");
+    node.setAttribute("href", "#");
+    node.innerHTML = value;
+
+    node.addEventListener("click", function () {
+        input.value = value;
+        dropdown.classList.remove("show");
+    });
+
+    return node;
+}
+
+function setup_filter_function() {
+
+    filter_function(document.getElementById("fra"), document.getElementById("myDropdownFra"));
+    filter_function(document.getElementById("til"), document.getElementById("myDropdownTil"));
+}
+
+function filter_function(input_id, dropdown_id) {
+    var filter, a, i, input, dropdown;
+    input = document.getElementById(input_id);
+    dropdown = document.getElementById(dropdown_id);
+    filter = input.value.toUpperCase();
+    a = dropdown.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            a[i].style.display = "";
+        } else {
+            a[i].style.display = "none";
+        }
+    }
+}
+
+function show_menu() {
+    show_menu_id("myDropdownFra", document.getElementById("fra"));
+    show_menu_id("myDropdownTil", document.getElementById("til"));
+}
+
+function show_menu_id(id, input) {
+    let el = document.getElementById(id);
+
+
+    if (input !== "") {
+
+        if (!el.classList.contains("show"))
+            el.classList.add("show");
+
+    }
+    if (input.value === "") {
+        el.classList.remove("show");
+    }
+}
+
+
+fra_input.setAttribute("autocomplete", "off");
+til_input.setAttribute("autocomplete", "off");
+
+setup_dropdown();
+
+$("#fra").keyup(function (event) {
+
+    filter_function("fra", "myDropdownFra");
+    show_menu_id("myDropdownFra", "fra");
+
+});
+
+$("#til").keyup(function () {
+    filter_function("til", "myDropdownTil");
+    show_menu_id("myDropdownTil", "til");
+});
+
+
