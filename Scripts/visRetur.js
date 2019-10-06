ed72fe5250de63c836af999644d1ccbb2d2ca8ea
Ozkan-Bicak <32769137+Ozkan-Bicak@users.noreply.github.com>
2019-10-06 15:21:26 +0200

    Default check på en vei knapp, og jquery for å gjemme tur retur

---
+++ Scripts/visRetur.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    console.log("yaaah");
+    $('input[type="radio"]').click(function () {
+        if ($(this).attr('id') == 'turRetur') {
+            $('#retur').show();
+        }
+
+        else {
+            $('#retur').hide();
+        }
+    });
+});
