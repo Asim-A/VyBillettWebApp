8ebfae31c56cc2de252885603f31d729001c50b4
Asim-A <asimabazi@hotmail.no>
2019-10-21 15:55:32 +0200

    resten

---
+++ VyBillettWebApp.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VyBillettWebApp.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VyBillettWebApp.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("50e39c3f-4ee7-4e18-a064-3b1c556f7905")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VyBillettWebApp.UnitTests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VyBillettWebApp.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DateTime now = DateTime.Now;
+
+            string[] nowString = {
+                now.Day.ToString(),
+                now.Month.ToString(),
+                now.Year.ToString(),
+                now.Hour.ToString(),
+                now.Minute.ToString(),
+                now.Second.ToString()
+            };
+
+
+            string dateFormatString = "dd/MM/yyyy HH:mm:ss";
+
+            string nowToString = now.ToString();
+
+            Console.WriteLine("Time: " + nowToString);
+
+
+            Thread.Sleep(1000);
+
+            DateTime later = DateTime.ParseExact(nowToString, dateFormatString, null);
+
+            string laterToString = later.ToString();
+
+            Console.WriteLine("Time later: " + laterToString);
+
+            
+
+        }
+    }
+}
