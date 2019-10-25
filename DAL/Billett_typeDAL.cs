5cb989ba687f7dc8cf56fcdd98070f03393c308c
Asim-A <asimabazi@hotmail.no>
2019-10-25 16:01:59 +0200

    hent billett_typer

---
+++ DAL/Billett_typeDAL.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class Billett_typeDAL
+    {
+
+        //public List<Model.Billett_type> GetBillett_Type()
+        //{
+        //    using (var db = new DB())
+        //    {
+        //        List<Model.Billett_type> list_billett_typer = db.Billett_typer.Select(element => new Billett_type()
+        //        {
+        //            billett_type = element.billett_type,
+        //            billett_pris = element.billett_pris
+        //        }).toList();
+
+        //        return list_billett_typer;
+        //    }
+        //}
+
+    }
+}
